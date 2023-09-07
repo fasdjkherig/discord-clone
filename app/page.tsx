8741e26f45933da18e610a478ab1ac14a502d5a0
fasdjkherig <farahat.farahat14141679@nursing.menofia.edu.eg>
2023-09-07 11:55:40 +0300

    third commit

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ import React from "react";
 const page = () => {
   return (
     <div>
-      <p className="text-3xl text-indigo-600">Hello Farahat</p>
+      <p className="text-3xl font-bold text-indigo-600">Hello Farahat</p>
       <Button>Click Me</Button>
     </div>
   );
