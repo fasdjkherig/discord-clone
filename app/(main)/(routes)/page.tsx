03340be76d5a03aed8243ffecdf95a776985c81f
fasdjkherig <farahat.farahat14141679@nursing.menofia.edu.eg>
2023-09-09 12:14:36 +0300

    6th commit

--- app/(main)/(routes)/page.tsx
+++ app/(main)/(routes)/page.tsx
@@ -1,7 +1,12 @@
+import { UserButton } from "@clerk/nextjs";
 import React from "react";
 
 const page = () => {
-  return <div>This is a protected route.</div>;
+  return (
+    <div>
+      <UserButton afterSignOutUrl="/" />
+    </div>
+  );
 };
 
 export default page;
