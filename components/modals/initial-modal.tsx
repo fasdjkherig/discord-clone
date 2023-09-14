e158c6120603b3709e2eba3fcfd9997f9e124964
fasdjkherig <farahat.farahat14141679@nursing.menofia.edu.eg>
2023-09-14 20:07:46 +0300

    16th commit

--- components/modals/initial-modal.tsx
+++ components/modals/initial-modal.tsx
@@ -23,6 +23,7 @@ import {
 import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
 import { FileUpload } from "@/components/file-upload";
+import { useRouter } from "next/navigation";
 
 const formSchema = z.object({
   name: z.string().min(1, {
@@ -35,6 +36,8 @@ const formSchema = z.object({
 
 const InitialModal = () => {
   const [isMounted, setIsMounted] = useState(false);
+
+  const router = useRouter();
 
   useEffect(() => {
     setIsMounted(true);
@@ -52,7 +55,13 @@ const InitialModal = () => {
 
   const onSubmit = async (values: z.infer<typeof formSchema>) => {
     try {
-      console.log(values);
+      await axios.post("/api/servers", values);
+
+      form.reset();
+
+      router.refresh();
+
+      window.location.reload();
     } catch (error) {
       console.log(error);
     }
