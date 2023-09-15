9c4023619319a1f7e1e1864e43191c08f6bca5ad
fasdjkherig <farahat.farahat14141679@nursing.menofia.edu.eg>
2023-09-15 07:43:02 +0300

    18th commi

---
+++ components/navigation/navigation-sidebar.tsx
@@ -0,0 +1,28 @@
+import React from "react";
+import { currentProfile } from "@/lib/current-profile";
+import { redirect } from "next/navigation";
+import { db } from "@/lib/db";
+
+const NavigationSidebar = async () => {
+  const profile = await currentProfile();
+
+  if (!profile) {
+    return redirect("/");
+  }
+
+  const servers = await db.server.findMany({
+    where: {
+      members: {
+        some: {
+          profileId: profile.id,
+        },
+      },
+    },
+  });
+  return (
+    <div className="space-y-4 flex flex-col items-center h-full text-primary w-full dark:bg-[#1E1F22] bg-[#E3E5E8] py-3">
+      navigation-sidebar
+    </div>
+  );
+};
+export default NavigationSidebar;
