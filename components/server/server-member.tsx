e000e1049a0539dec3ec68a8d2c665b9ba304a2f
fasdjkherig <farahat.farahat14141679@nursing.menofia.edu.eg>
2023-09-29 22:48:55 +0300

    43th commit

---
+++ components/server/server-member.tsx
@@ -0,0 +1,23 @@
+"use client";
+
+import React from "react";
+import { Member, MemberRole, Profile, Server } from "@prisma/client";
+import { ShieldAlert, ShieldCheck } from "lucide-react";
+interface ServerMemberProps {
+  member: Member & { profile: Profile };
+  server: Server;
+}
+
+const roleIconMap = {
+  [MemberRole.GUEST]: null,
+  [MemberRole.MODERATOR]: (
+    <ShieldCheck className="h-4 w-4 ml-2 text-indigo-500" />
+  ),
+  [MemberRole.ADMIN]: <ShieldAlert className="h-4 w-4 ml-2 text-rose-500" />,
+};
+
+const ServerMember = ({ member, server }: ServerMemberProps) => {
+  return <div>ServerMember</div>;
+};
+
+export default ServerMember;
