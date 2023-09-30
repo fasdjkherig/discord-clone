3dc29b1f55915efbe8695456239e3c25ed913cad
fasdjkherig <farahat.farahat14141679@nursing.menofia.edu.eg>
2023-09-30 08:41:58 +0300

    44th commit

--- components/modals/create-channel-modal.tsx
+++ components/modals/create-channel-modal.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import React from "react";
+import React, { useEffect } from "react";
 import qs from "query-string";
 import axios from "axios";
 import * as z from "zod";
@@ -47,20 +47,30 @@ const formSchema = z.object({
 });
 
 const CreateChannelModal = () => {
-  const { isOpen, onClose, type } = useModal();
+  const { isOpen, onClose, type, data } = useModal();
 
   const router = useRouter();
   const params = useParams();
 
   const isModalOpen = isOpen && type === "createChannel";
 
+  const { channelType } = data;
+
   const form = useForm({
     defaultValues: {
       name: "",
-      type: ChannelType.TEXT,
+      type: channelType || ChannelType.TEXT,
     },
     resolver: zodResolver(formSchema),
   });
+
+  useEffect(() => {
+    if (channelType) {
+      form.setValue("type", channelType);
+    } else {
+      form.setValue("type", ChannelType.TEXT);
+    }
+  }, [channelType, form]);
 
   const isLoading = form.formState.isSubmitting;
 
--- components/server/server-member.tsx
+++ components/server/server-member.tsx
@@ -3,6 +3,10 @@
 import React from "react";
 import { Member, MemberRole, Profile, Server } from "@prisma/client";
 import { ShieldAlert, ShieldCheck } from "lucide-react";
+import { cn } from "@/lib/utils";
+import { useParams, useRouter } from "next/navigation";
+import UserAvatar from "@/components/user-avatar";
+
 interface ServerMemberProps {
   member: Member & { profile: Profile };
   server: Server;
@@ -17,7 +21,33 @@ const roleIconMap = {
 };
 
 const ServerMember = ({ member, server }: ServerMemberProps) => {
-  return <div>ServerMember</div>;
+  const params = useParams();
+  const router = useRouter();
+
+  const icon = roleIconMap[member.role];
+  return (
+    <button
+      className={cn(
+        "group px-2 py-2 rounded-md flex items-center gap-x-2 w-full hover:bg-zinc-700/10 dark:hover:bg-zinc-700/50 transition mb-1",
+        params?.memberId === member.id && "bg-zinc-700/20 dark:bg-zinc-700"
+      )}
+    >
+      <UserAvatar
+        src={member.profile.imageUrl}
+        className="h-8 w-8 md:h-8 md:w-8"
+      />
+      <p
+        className={cn(
+          "font-semibold text-sm text-zinc-500 group-hover:text-zinc-600 dark:text-zinc-400 dark:group-hover:text-zinc-300 transition",
+          params?.memberId === member.id &&
+            "text-primary dark:text-zinc-200 dark:group-hover:text-white"
+        )}
+      >
+        {member.profile.name}
+      </p>
+      {icon}
+    </button>
+  );
 };
 
 export default ServerMember;
